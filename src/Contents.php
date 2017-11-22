607d2f86898a11b5641b9ca1e2597cf695051b6c
Marcin Kozak <marcin.ko@modulesgarden.com>
2017-11-22 09:41:59 +0100

    CHG: commented code for later investigation

--- src/Contents.php
+++ src/Contents.php
@@ -21,6 +21,7 @@
 namespace Antares\Notifications;
 
 use Antares\Notifications\Model\NotificationContents;
+use Illuminate\Support\Collection;
 
 class Contents
 {
@@ -36,21 +37,23 @@ class Contents
      * Fetch notifications to the instance variable.
      */
     protected function fetch() {
-        $this->notifications = NotificationContents::query()
-            ->select([
-                'tbl_notification_contents.id',
-                'tbl_languages.code',
-                'tbl_notification_contents.title',
-                'tbl_notification_contents.content',
-            ])
-            ->leftJoin('tbl_notifications', 'tbl_notification_contents.notification_id', '=', 'tbl_notifications.id')
-            ->leftJoin('tbl_languages', 'tbl_notification_contents.lang_id', '=', 'tbl_languages.id')
-            ->leftJoin('tbl_notification_types', 'tbl_notifications.type_id', '=', 'tbl_notification_types.id')
-            ->leftJoin('tbl_notifications_stack', 'tbl_notifications.id', '=', 'tbl_notifications_stack.notification_id')
-            ->where('tbl_notification_types.name', 'admin')
-            ->whereNotNull('tbl_notification_contents.content')
-            ->where('tbl_notification_contents.content', '<>', '')
-            ->get();
+        $this->notifications = new Collection();
+
+//        $this->notifications = NotificationContents::query()
+//            ->select([
+//                'tbl_notification_contents.id',
+//                'tbl_languages.code',
+//                'tbl_notification_contents.title',
+//                'tbl_notification_contents.content',
+//            ])
+//            ->leftJoin('tbl_notifications', 'tbl_notification_contents.notification_id', '=', 'tbl_notifications.id')
+//            ->leftJoin('tbl_languages', 'tbl_notification_contents.lang_id', '=', 'tbl_languages.id')
+//            ->leftJoin('tbl_notification_types', 'tbl_notifications.type_id', '=', 'tbl_notification_types.id')
+//            ->leftJoin('tbl_notifications_stack', 'tbl_notifications.id', '=', 'tbl_notifications_stack.notification_id')
+//            ->where('tbl_notification_types.name', 'admin')
+//            ->whereNotNull('tbl_notification_contents.content')
+//            ->where('tbl_notification_contents.content', '<>', '')
+//            ->get();
     }
 
     /**
